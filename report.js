f927faa84068e7c80d3b73a56d5e04e184d0b047
Ridoyabdulla <ridoy.ridoy005@gmail.com>
2025-07-16 02:44:29 +0600

    remove pmatkey from code and added to .env

--- report.js
+++ report.js
@@ -1,8 +1,9 @@
 const newman = require ('newman');
+require('dotenv').config();
 
 newman.run({
 
-    collection:`https://api.postman.com/collections/45503194-a644143e-5f66-4c61-a796-838dba5ca87e?access_key=PMAT-01K07Q1Q3CPY856X5V9FB499PE`,
+    collection:`https://api.postman.com/collections/45503194-a644143e-5f66-4c61-a796-838dba5ca87e?access_key=${process.env.pmatKey}`,
 
     reporters: 'htmlextra',
     iterationCount: 1,
